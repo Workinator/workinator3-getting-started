d31ab3bada5890478c068014947bd295effb7a68
Jay Allard <jay_allard@ultimatesoftware.com>
2018-03-29 22:41:07 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Workinator3 - Getting Started
+
+This project contains scripts and (eventually) documentation on how to work with the other workinator 3 projects.
+
+`stuff.py` was written using Pyton 3.6.
+
+It iterates the workinator folders and executes various commands.
+
+IE: `stuff status` executes `git status` in each directory.
+
+Support commands
+* pull
+* commit
+    * git add --all
+    * git commit -m "TODO"
+    * git push
+* TODO: clone
+
---
+++ stuff.py
@@ -0,0 +1,41 @@
+import sys
+import os
+from subprocess import call
+
+projects = [
+    "workinator3",
+    "workinator3-consumer",
+    "workinator3-consumer-rabbitmq",
+    "workinator3-coordinator",
+    "workinator3-coordinator-mongodb",
+    "workinator3-demo-inprocess",
+    "workinator3-demo-rabbitmq"
+]
+
+command = sys.argv[1]
+
+startFolder = os.path.dirname(sys.argv[0])
+startFolder = os.path.abspath(os.path.join(startFolder, ".."))
+
+print("----------------------------------------------------------------------------------")
+print("Workinator3 Project Folder: " + startFolder)
+print("----------------------------------------------------------------------------------")
+
+commands = []
+if command == "pull":
+    commands.append("git pull")
+elif command == "commit":
+    commands.append("git add --all")
+    commands.append("git commit -m \"todo\"")
+    commands.append("git push")
+elif command == "status":
+    commands.append("git status")
+    
+for project in projects:
+    cwd = os.path.join(startFolder, project)
+    print(cwd)
+    for c in commands:
+        call(c, cwd=cwd)
+
+
+
